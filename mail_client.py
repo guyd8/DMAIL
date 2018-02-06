5f6edde8b5273108138c0a88a1cac2543f2994de
Guy Davidovich <guydavid3399@walla.com>
2018-02-06 09:04:12 +0200

    adding files to the repository

---
+++ mail_client.py
@@ -0,0 +1,49 @@
+__author__ = 'Guy Davidovich'
+
+from socket import *
+
+smtp_server_ip = '127.0.0.1'
+smtp_server_port = 30000
+imap_server_ip = '127.0.0.1'
+imap_server_port = 24000
+
+
+def receive_messages():
+    username = raw_input('Enter your name\n')
+    receiver_client = socket()
+    receiver_client.connect((imap_server_ip, imap_server_port))
+    receiver_client.send(username)
+    number_of_messages = int(receiver_client.recv(1024))
+    for i in range(number_of_messages):
+        message = receiver_client.recv(1024)
+        print message
+    receiver_client.close()
+
+
+def send_message(message):
+    sender_client = socket()
+    sender_client.connect((smtp_server_ip, smtp_server_port))
+    sender_client.send(message)
+    sender_client.close()
+
+
+def build_message():
+    message = ''
+    message += 'From: ' + raw_input('Enter your name\n') + '\n'
+    message += 'To: ' + raw_input('Enter the recipient\'s name\n') + '\n'
+    message += 'Title: ' + raw_input('Enter title\n') + '\n'
+    message += 'Content: ' + raw_input('Enter message content\n') + '\n'
+    print message
+    print message.split('\n')
+    send_message(message)
+
+
+def main():
+    response = raw_input('Would you like to send a message or receive a message?\n')
+    if response == 'send':
+        build_message()
+    elif response == 'receive':
+        receive_messages()
+
+if __name__ == '__main__':
+    main()
---
+++ smtp_server.py
@@ -0,0 +1,39 @@
+__author__ = 'Guy Davidovich'
+
+from socket import *
+
+smtp_port = 30000
+imap_server_ip = '127.0.0.1'
+smtp_to_imap_port = 27000
+
+
+def send_to_imap_server(message):
+    sender = socket()
+    sender.connect((imap_server_ip, smtp_to_imap_port))
+    sender.send(message)
+    sender.close()
+    print 'message sent'
+
+
+def receive_message_from_client(client_socket):
+    message = client_socket.recv(1024)
+    print message
+    client_socket.close()
+    return message
+
+
+def main():
+    server_socket = socket()
+    server_socket.bind(('0.0.0.0', smtp_port))
+    server_socket.listen(1)
+    while True:
+        client_socket, address = server_socket.accept()
+        print 'client connected'
+        message = receive_message_from_client(client_socket)
+        send_to_imap_server(message)
+
+    server_socket.close()
+
+
+if __name__ == '__main__':
+    main()
